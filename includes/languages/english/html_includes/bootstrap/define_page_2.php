95ddf9abc0b2ba5c2c3521cdd4a87b20ca2396c0
brittainmark <gitbrit@inner-light.co.uk>
2022-10-23 11:00:00 +0100

    Language File Changes

---
+++ includes/languages/english/html_includes/bootstrap/define_page_2.php
@@ -0,0 +1,298 @@
+<ul id="top">
+	<li><a
+		href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>#PayPal"><strong>What
+				is PayPal and how does it work?</strong></a></li>
+	<li><a
+		href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>#BankTransfer"><strong>Paying
+				for your purchases by Direct Bank Transfer</strong></a></li>
+	<li><a
+		href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>#CreditCard"><strong>Paying
+				for your purchases using a Credit or Debit card</strong></a></li>
+	<li><a
+		href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>#PayPalAccount"><strong>Paying
+				for your purchases using a PayPal account</strong></a></li>
+	<li><a
+		href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>#Cheque"><strong>Paying
+				for your purchases using a Cheque, Postal Order or Money Order</strong></a></li>
+	<li><a
+		href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>#Shop"><strong>Do
+				you have a shop? or can we visit you?</strong></a></li>
+	<li><a
+		href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>#Gift"><strong>Can
+				I buy an item as a gift for someone else?</strong></a></li>
+	<li><a
+		href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>#Source"><strong>How
+				do you source your stock?</strong></a></li>
+	<li><a
+		href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>#Refund"><strong>Refunds
+				- changed your mind?</strong></a></li>
+	<li><a
+		href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>#Damaged"><strong>Refunds
+				- items damaged in transit.</strong></a></li>
+	<li><a
+		href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>#Other"><strong>Refunds
+				- other items returned to us.</strong></a></li>
+	<li><a
+		href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>#Lost"><strong>I
+				have not received my delivery.</strong></a></li>
+</ul>
+<p id="PayPal">
+	<strong>What is PayPal and how does it work?</strong>
+</p>
+<p>
+	PayPal is a safer, simpler way to send and receive money online. It's
+	free to sign up for a PayPal account, and you can pay for purchases
+	either with money held in your PayPal account balance, or by credit
+	card, debit card or bank transfer. You can then simply select PayPal as
+	your payment method when you shop online, and then log in to your
+	account with your email address and password to confirm the payment.
+	PayPal never shares your financial information with sellers or
+	retailers: for more information visit <a href="http://www.paypal.co.uk"
+		target="blank">www.paypal.co.uk.</a><br> <a
+		href="index.php?main_page=page_2">Top</a><br>
+<hr>
+<p id="BankTransfer">
+	<strong>Paying for your purchases by Direct Bank Transfer:</strong>
+</p>
+<p>
+	Once you have added all the items you wish to purchase to your basket,
+	follow the link to the checkout. At "Payment Method" select "Direct
+	Bank Transfer": then, after you have accepted the terms and conditions,
+	and confirmed that your order details are correct, you will be given
+	instructions on making the transfer (which will also be emailed to
+	you).<br> If you use this method, the items in your basket will be
+	reserved for 7 days to await confirmation of your transfer; and will be
+	despatched once the appropriate funds have reached our account.<br>
+	PLEASE GIVE YOUR POSTCODE AND NAME AS THE REFERENCE so that we can link
+	your payment with your order.
+</p>
+<a href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>">Top</a>
+<hr>
+<p id="CreditCard">
+	<strong>Paying for your purchases using a Credit or Debit card:</strong>
+</p>
+<p>We are fully aware of the risks involved with card transactions over
+	the internet, so for both parties' safety and peace of mind we process
+	all credit and debit card payments using Square, which is our trusted
+	payment processing facility. You do not need to be registered with
+	Square in order to use this service.</p>
+<p>
+	Once you have added all the items you wish to purchase to your basket,
+	follow the link to the checkout. At "Payment Method" select "Credit /
+	Debit Card"; and complete the fields with the required information
+	(your card number; CVV number; card expiry date; and your postcode).
+	NB: your card must be registered to the billing address given.<br>
+	Payment will not be requested until after you have accepted the terms
+	and conditions, and confirmed that your order details are correct.
+</p>
+<a href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>">Top</a>
+<hr>
+<p id="PayPalAccount">
+	<strong>Paying for your purchases using a PayPal account:</strong>
+</p>
+<p>Once you have added all the items you wish to purchase to your
+	basket, follow the link to the checkout. At "Payment Method" select
+	"Use my PayPal account": then, after you have accepted the terms and
+	conditions, and confirmed that your order details are correct, you will
+	be taken to the appropriate PayPal page to complete your transaction.</p>
+<hr>
+<a href="index.php?main_page=page_2">Top</a>
+<p id="Cheque">
+	<strong>Paying for your purchases using a Cheque, Postal Order or Money
+		Order:</strong>
+</p>
+<p>
+	Once you have added all the items you wish to purchase to your basket,
+	follow the link to the checkout. At "Payment Method" select "Cheque,
+	money order, postal order": then, after you have accepted the terms and
+	conditions, and confirmed that your order details are correct, you will
+	be given instructions for mailing your payment to us.<br> If you use
+	this method, the items in your basket will be reserved for 7 days to
+	await your payment; and will be despatched once the appropriate funds
+	have cleared our bank account, which may take up to a week to happen.<br>
+	PLEASE ENSURE YOU INCLUDE YOUR NAME AND ADDRESS WITH YOUR PAYMENT so
+	that we can link your payment with your order.
+</p>
+<a href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>">Top</a>
+<hr>
+<p id="Shop">
+	<strong>Do you have a shop? or can we visit you?</strong>
+</p>
+<p>
+	We operate our business directly from our home address and do not have
+	a high-street outlet whereby you can simply &quot;drop in&quot;.
+	However we are always happy to accommodate those who wish to browse and
+	buy in person, but need to arrange any such visits to fit in with our
+	teaching and therapy commitments. If you would like to call and browse
+	through our VERY extensive stock and select crystals for yourself,
+	please telephone us (01239) 682676 to arrange a visit. NB: you will
+	reach our &quot;Phone Guardian&quot; that will ask you to state your
+	name before you are put through.<br> Payment at the time of your visit
+	can be arranged by credit / debit card IF you have a PayPal account OR
+	by cash.<br> One of the advantages of visiting us is that you can take
+	advantage of our expertise (we teach Crystal Healing practitioner
+	courses and Crystal workshops and are also qualified Crystal Healers
+	and Energy Workers) and discuss with us, if need be, what might be
+	right for your needs. You may even be offered a cup of tea! Another
+	advantage of a personal shopping visit is that you will not have to pay
+	any delivery costs, and you may even be able to negotiate a one-off
+	special deal on &quot;large&quot; purchases.<br> Please note that all
+	goods sold in this way will be &quot;as seen&quot;, and thus refunds
+	(if you later change your mind) will not be possible.<br> <a
+		href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>">Top</a><br>
+<hr>
+<p id="Gift">
+	<strong>Can I buy an item as a gift for someone else?</strong>
+</p>
+<p>
+	YES! When completing your order form you are asked if the delivery
+	address is other than your own. The item(s) will then be despatched to
+	that address, once all other criteria have been satisfied. If you wish
+	a message for the recipient (such as: &quot;Happy Birthday Frank, with
+	love from Jane&quot;) to be included with the item(s), simply enter it
+	in the box that is headed: &quot;delivery message; special
+	instructions; comments about your order&quot;.<br> We also can provide
+	credit notes / gift vouchers if you wish to give a special gift, yet
+	allow the recipient to make their own selections. Eventually you will
+	be able to buy these &quot;on-line&quot;, but at the moment if you wish
+	to arrange to buy one of these please contact us to discuss what you
+	need.<br> <a
+		href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>">Top</a><br>
+<hr>
+<p id="Source">
+	<strong>How do you source your stock?</strong>
+</p>
+<p>
+	We purchase direct from wholesalers and private suppliers within the
+	UK, and in most cases buy items we can hand-pick in person, ensuring
+	the quality and integrity of what we offer to others. Wherever possible
+	we select crystals from the most ethical and knowledgeable suppliers,
+	whilst also choosing them through our experience of quality and price.
+	Most of the wholesalers we use specialise in certain crystals and / or
+	source them from specific countries, buying direct from the mines or
+	their immediate distributors. This means that we are able to acquire
+	the best crystals we can find and sell them to our customers, usually
+	at &quot;below high street&quot; prices.<br> <a
+		href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>">Top</a><br>
+<hr>
+<p id="Refund">
+	<strong>Refunds - changed your mind?</strong>
+<p>We make every effort to be honest when describing and photographing
+	each item for sale. However there may be circumstances where you are
+	not happy with an item you have purchased and wish to return it to us.
+	In these cases:
+<ul>
+	<li>please notify us immediately of your intentions, stating clearly
+		the reason for its return</li>
+	<li>return the item to us within 28 days of the purchase date, making
+		every effort to ensure it reaches us safely and in the same condition
+		in which it reached you. You will be responsible for all postage and
+		packing costs for the item's safe return to us: &quot;Inner Light
+		Crystals&quot; will not accept any &quot;delivery unpaid&quot; items.</li>
+</ul>
+Once we have received the item and checked its condition is unchanged we
+will arrange for a refund of the item's purchase price plus our (outward
+bound) standard (lowest) postal cost for that item and the original
+packing fee of &pound;1. If you chose a more expensive delivery rate
+(e.g. next day delivery) when purchasing the item we regret that these
+additional postal costs will not be refunded.
+<br>
+<a href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>">Top</a>
+<br>
+<hr>
+<p id="Damaged">
+	<strong>Refunds - items damaged in transit.</strong>
+<p>
+	Your delivery options will have ensured that all despatches are covered
+	by Royal Mail's insurance terms and conditions. If any item has
+	obviously been damaged in the delivery process, please contact us
+	immediately, stating the problem, before you send the item(s) back. We
+	will then let you know the procedure to be followed regarding its
+	return and what our refund of your purchase and costs will be.<br> <a
+		href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>">Top</a><br>
+<hr>
+<p id="Other">
+	<strong>Refunds - other items returned to us:</strong>
+<ul>
+	<li>Duties / import duties unpaid, delivery refused:<br> If you do not
+		pay the duties requested by your country of receipt and the parcel is
+		returned to us for this reason, you will be responsible for both the
+		outward AND inward bound delivery costs: these will be deducted from
+		the invoiced sum, and you will be refunded the difference (assuming
+		the item(s) are returned to us in the same condition in which they
+		were despatched).
+	</li>
+	<li>Items returned as 'undeliverable':<br> Please check the delivery
+		address is correct before placing an order to avoid items being
+		returned to us 'undelivered'. We are not responsible for checking
+		postal addresses, and both the outward AND inward bound delivery costs
+		will be deducted from the invoiced sum before refund is made.
+	</li>
+</ul>
+<br>
+<a href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>">Top</a>
+<br>
+<hr>
+<p id="Lost">
+	<strong>I have not received my delivery.</strong>
+<p>
+	<strong class="cvs">Coronavirus:</strong> Please bear in mind that
+	deliveries can be affected by local lock down measures and/or staffing
+	issues, and (in the case of destinations outside the UK) by the
+	availability of sufficient freight carrying capacity on aircraft.
+	Although every effort is made to deliver items within the timescales
+	given per your chosen shipping method, at present these cannot be
+	absolutely guaranteed and so your order may arrive a little later than
+	expected.
+</p>
+<p>Royal Mail supply the following guidelines for determining if / when
+	an item will be considered missing or lost in transit. Please follow
+	these guidelines according to the method of delivery you chose when
+	placing your order. If you have still not received your expected
+	delivery after the relevant time has elapsed, please contact us and we
+	will investigate and / or arrange a refund as appropriate.</p>
+<div id="GPOLate">
+	<table>
+		<thead>
+			<tr>
+				<td>Product</td>
+				<td>Due Date</td>
+				<td>Considered lost in transit</td>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>1st class</td>
+				<td>Next working day after posting</td>
+				<td>10 working days after due date</td>
+			</tr>
+			<tr>
+				<td>2nd class</td>
+				<td>3&nbsp; working days after posting</td>
+				<td>10 working days after due date</td>
+			</tr>
+			<tr>
+				<td>Standard Parcels</td>
+				<td>3&nbsp; working days after posting</td>
+				<td>10 working days after due date</td>
+			</tr>
+			<tr>
+				<td>Special Delivery</td>
+				<td>Per delivery specification</td>
+				<td>5 working days after due date</td>
+			</tr>
+			<tr>
+				<td>International (Europe)</td>
+				<td>Per delivery specification</td>
+				<td>20 working days after due date</td>
+			</tr>
+			<tr>
+				<td>International (Rest of World)</td>
+				<td>Per delivery specification</td>
+				<td>25 working days after due date</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+<a href="<?php echo zen_href_link(FILENAME_PAGE_2, '', 'SSL');?>">Top</a>
+<p>&nbsp;
