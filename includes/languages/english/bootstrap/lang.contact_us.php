9da9b2a1d00ec31ef65d4f0e7477f0ebb3dbf886
brittainmark <gitbrit@inner-light.co.uk>
2025-01-23 08:45:33 +0000

    Update lang.contact_us.php
    add missing subject check

--- includes/languages/english/bootstrap/lang.contact_us.php
+++ includes/languages/english/bootstrap/lang.contact_us.php
@@ -1,6 +1,7 @@
 <?php
 $define = [
     'EMAIL_SUBJECT' => 'Website Enquiry from ' . STORE_NAME,
+    'ENTRY_EMAIL_SUBJECT_CHECK_ERROR' => 'Sorry, is your subject correct? Our system requires a minimum of ' . ENTRY_FIRST_NAME_MIN_LENGTH . ' characters. Please try again.',
 ];
 
 return $define;
